b4403310022373620f96491c7535b19db630a0fc
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-08 16:53:41 -0500

    Initial commit.

---
+++ even-fibonacci.js
@@ -0,0 +1,19 @@
+// Fibonacci number = previous + one before previous
+// Normal Mode
+
+var i;
+var sum = 0;
+var curVal = 0;
+var maxVal = 4000000;
+var fSeq = [0, 1];
+
+for(i=2; curVal < maxVal; i++)
+{
+    fSeq[i] = fSeq[i-2] + fSeq[i-1];
+    if (fSeq[i] % 2 === 0) {
+        sum = sum + fSeq[i];
+        console.log(fSeq[i]);
+    }
+    curVal = fSeq[i];
+}
+console.log("\nSum of all the even numbers in sequence less than " + maxVal + " = " + sum);
