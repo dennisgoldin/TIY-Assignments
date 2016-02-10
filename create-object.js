76040880cf62bd4c8e6a72bb8cf562285d7ca5fc
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-10 10:47:23 -0500

    Add file create-object.js.

---
+++ create-object.js
@@ -0,0 +1,12 @@
+// create an object and output each key value to console
+var myObject = {
+    myTitle: "Dip",
+    myServings: 2,
+    myIngredients: ["sour cream", "spices", "herbs"]
+}
+console.log(myObject.myTitle);
+console.log(myObject.myServings);
+console.log("Ingredients:")
+console.log(myObject.myIngredients[0]);
+console.log(myObject.myIngredients[1]);
+console.log(myObject.myIngredients[2]);
