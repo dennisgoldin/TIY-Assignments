180f1e6d453dc1eae23e7f1fb8cf6069d26190ff
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-11 10:05:26 -0500

    Add code challenge calculate-supply.js

---
+++ calculate-supply.js
@@ -0,0 +1,13 @@
+// code challenge
+function calculateSupply (myAge, myDailyAmount) {
+    var myMaxAge = 65;  // constant
+    // console.log(myMaxAge);
+    var myTimeSpan = myMaxAge - myAge;
+    // console.log(myTimeSpan);
+    var myTotalAmount = myTimeSpan * myDailyAmount;
+    console.log("You will need " + myTotalAmount + " snacks to last you until the ripe old age of " + myMaxAge + ".");
+}
+
+calculateSupply(40, 2);
+calculateSupply(45, 3);
+calculateSupply(50, 4);
