bd84ae7b67f6b25f892bb54ac0aa067198caa3eb
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-25 10:38:41 -0500

    Code challenge

--- top-choices.js
+++ top-choices.js
@@ -1,13 +1,14 @@
 // code challenge 02-25-2016
 
 var myArray = ["red", "blue", "green"];
+var mySuffix = { 0 : "st", 1 : "nd", 2 : "rd"}
 
 for (var i = 0; i < myArray.length; i++) {
     if (myArray[i] === "red") {
-        console.log("My 1st choice is " + myArray[i] + ".");
+        console.log("My 1" + mySuffix[i] + " choice is " + myArray[i] + ".");
     } else if (myArray[i] === "blue") {
-        console.log("My 2nd choice is " + myArray[i] + ".");
+        console.log("My 2" + mySuffix[i] + " choice is " + myArray[i] + ".");
     } else {
-        console.log("My 3rd choice is " + myArray[i] + ".");
+        console.log("My 3" + mySuffix[i] + " choice is " + myArray[i] + ".");
     }
 }
