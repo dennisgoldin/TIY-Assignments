ea91079aba13e242f1efc3b07de8643a0b68513d
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-16 10:52:38 -0400

    Included Byron's awesome answer in comments

--- longest-country-name.js
+++ longest-country-name.js
@@ -10,6 +10,17 @@ function longestCountryName(arr) {
     return longestName;
 }
 
-
 console.log(longestCountryName(["Germany", "Australia", "United States of America"]));
 console.log(longestCountryName(["Germany", "Australia", "Switzerland"]));
+
+
+// Byron's awesome answer
+// function longest_country_name(arr){
+//   console.log(arr);
+//   arr.sort(function (a, b) { return b.length - a.length })[0];
+//   console.log(arr);
+//   console.log(arr[0]);
+// }
+// ​
+// ​
+// longest_country_name(['Abyssinia', 'Bechuanaland', 'Grain Coast', 'Songhay', 'Stellaland', 'Kingdom of Tungning', 'British North Borneo', 'Gorkhali', 'Kampuchea', 'Mustang, Kingdom of', 'Inini', 'Confœderatio Helvetica', 'Pridnestrovia', 'Spitzbergen'])
