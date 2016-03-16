81410ac073d8c875e1577fbfbc7e87ddf7c08f25
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-16 10:28:46 -0400

    Initial commit

---
+++ longest-country-name.js
@@ -0,0 +1,14 @@
+// code challenge 03-16-2016
+
+function longestCountryName(arr) {
+    var longestName = "";
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length > longestName.length) {
+            longestName = arr[i];
+        }
+    }
+    return longestName;
+}
+
+console.log(longestCountryName(["Germany", "Australia", "United States of America"]));
+console.log(longestCountryName(["Germany", "Australia", "Switzerland"]));
